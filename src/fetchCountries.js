1078698bffbd3948fa59f7156c22a2aa36c386c5
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-27 06:25:59 +0200

    Доданий index.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,8 @@ const BASE_URL = 'https://restcountries.com';
 
 export const fetchCountries = name => {
   return fetch(
-    `${BASE_URL}/v3.1/name/${name}&name.official&capital&population&flags.svg&languages`
+    //   `${BASE_URL}/v3.1/name/${name}&name.official&capital&population&flags.svg&languages`
+    `${BASE_URL}/v3.1/name/${name}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -6,3 +6,26 @@ const DEBOUNCE_DELAY = 300;
 const searchFormEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
+
+const onSearchFormInput = event => {
+  event.preventDefault();
+
+  const searchedQuery = searchFormEl.value.trim();
+  //   console.log(searchedQuery);
+
+  fetchCountries(searchedQuery)
+    .then(data => {
+      console.log(data);
+
+      countryListEl.innerHTML = createCountryList(data);
+      countryInfoEl.innerHTML = createCountryInfoCard(data);
+    })
+    .catch(err => {
+      if (err.message === '404') {
+        // alert('Oops, there is no country with that name');
+        console.log('Oops');
+      }
+    });
+};
+
+searchFormEl.addEventListener('input', onSearchFormInput);
